8bb99a9617177c2831bcb44fb5b1c07d58dbca00
nrilsy <ulusoynuri3@gmail.com>
2025-02-10 13:20:43 +0300

    simple commander api integration

---
+++ bumperbot_navigation/bumperbot_navigation/simple_commander.py
@@ -0,0 +1,18 @@
+# #!/usr/bin/env python3
+# import rclpy
+# from nav2_simple_commander.robot_navigator import BasicNavigator
+# from rclpy.node import Node
+
+# class SimpleCommanderNode(Node):
+#     def __init__(self):
+#         super().__init__('simple_commander_node')
+#         self.subscriber_ = self.create_subscription()
+# def main(args=None):
+#     rclpy.init(args=args)
+#     node = SimpleCommanderNode()
+#     rclpy.spin(node)
+#     node.destroy_node()
+#     rclpy.shutdown()
+
+# if __name__ == '__main__':
+#     main()
